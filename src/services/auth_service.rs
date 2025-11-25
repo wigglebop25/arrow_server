13d8ef9659cd1eecaf07d4b5f181dea0a0309a82
jm-nitrolinux <mac2x.manos@gmail.com>
2025-11-25 16:48:10 +0800

    FEAT: Make auth_service.rs async

--- src/services/auth_service.rs
+++ src/services/auth_service.rs
@@ -2,6 +2,7 @@ use crate::services::errors::AuthServiceError;
 use argon2::password_hash::SaltString;
 use argon2::password_hash::rand_core::OsRng;
 use argon2::{Argon2, PasswordHasher, PasswordVerifier};
+use tokio::task;
 
 pub struct AuthService;
 // TODO: Test this service
@@ -11,13 +12,19 @@ impl AuthService {
     }
 
     pub async fn hash_password(&self, password: &str) -> Result<String, AuthServiceError> {
-        let argon2 = Argon2::default();
-        let salt = SaltString::generate(&mut OsRng);
+        let password = password.to_string();
 
-        match argon2.hash_password(password.as_bytes(), &salt) {
-            Ok(hash) => Ok(hash.to_string()),
-            Err(_) => Err(AuthServiceError::HashingError),
-        }
+        task::spawn_blocking(move || {
+            let argon2 = Argon2::default();
+            let salt = SaltString::generate(&mut OsRng);
+
+            match argon2.hash_password(password.as_bytes(), &salt) {
+                Ok(hash) => Ok(hash.to_string()),
+                Err(_) => Err(AuthServiceError::HashingError),
+            }
+        })
+        .await
+        .map_err(|_| AuthServiceError::HashingError)?
     }
 
     pub async fn verify_password(
@@ -25,18 +32,25 @@ impl AuthService {
         password: &str,
         hash: &str,
     ) -> Result<bool, AuthServiceError> {
-        let parsed_hash = match argon2::password_hash::PasswordHash::new(hash) {
-            Ok(h) => h,
-            Err(_) => return Err(AuthServiceError::VerificationError),
-        };
+        let password = password.to_string();
+        let hash = hash.to_string();
 
-        let argon2 = Argon2::default();
+        task::spawn_blocking(move || {
+            let parsed_hash = match argon2::password_hash::PasswordHash::new(&hash) {
+                Ok(h) => h,
+                Err(_) => return Err(AuthServiceError::VerificationError),
+            };
 
-        match argon2.verify_password(password.as_bytes(), &parsed_hash) {
-            Ok(_) => Ok(true),
-            Err(argon2::password_hash::Error::Password) => Ok(false),
-            Err(_) => Err(AuthServiceError::VerificationError),
-        }
+            let argon2 = Argon2::default();
+
+            match argon2.verify_password(password.as_bytes(), &parsed_hash) {
+                Ok(_) => Ok(true),
+                Err(argon2::password_hash::Error::Password) => Ok(false),
+                Err(_) => Err(AuthServiceError::VerificationError),
+            }
+        })
+        .await
+        .map_err(|_| AuthServiceError::VerificationError)?
     }
 }
 
