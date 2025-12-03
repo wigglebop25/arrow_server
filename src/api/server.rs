4fc8d22fdb6ab8c5c0aa1cd4323da9c82f00137a
jm-eospc <mac2x.manos@gmail.com>
2025-12-03 12:28:38 +0800

    FEAT: Add tower-http for CORS support and update server configuration

--- src/api/server.rs
+++ src/api/server.rs
@@ -1,13 +1,16 @@
 use crate::controllers::user_controller::{login, register_user};
 use axum::Router;
 use axum::routing::{get, post};
+use tower_http::cors::{Any, CorsLayer};
 use std::net::SocketAddr;
+use azure_storage::StorageCredentialsInner::Anonymous;
 use tokio::net::TcpListener;
 
 // TODO: Implement the API module
 // TODO: Add swagger documentation
 // TODO: Rework API implementation use https://github.com/sheroz/axum-rest-api-sample as reference
 pub async fn start() {
+    let cors_layer = CorsLayer::new().allow_origin(Any);
     let router = Router::new()
         .route("/api", get(|| async { "Arrow Server API is running!" }))
         .route("/api/v1/users/register", post(register_user))
